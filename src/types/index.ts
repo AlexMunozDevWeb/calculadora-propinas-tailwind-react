20487f88254b0059160094eab2c5ec20b9216414
Alex <alemumu207@gmail.com>
2024-06-06 18:29:57 +0200

    Fixing proyect

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,8 @@ export type MenuItem = {
   name: string,
   price: number,
 }
+
+//Cogemos todo lo que tenemos en MenuItem y se lo asignamos a OrderItem
+export type OrderItem = MenuItem & {
+  quantity: number,
+}
