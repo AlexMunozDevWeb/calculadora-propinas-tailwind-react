bd07c7491cbbe6ddca4b41ce1b01c4eb1e178c35
Alex <alemumu207@gmail.com>
2024-06-06 18:47:56 +0200

    Añadiendo elemento del state

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,9 +6,14 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item : MenuItem) => {
-      console.log(item);
+      //No le especificamos el tipo porque se lo colocamos en el generic del state <OrderItem[]>([])
+      const newItem = {...item, quantity: 1}
+      setOrder([...order, newItem])
       
     }
+
+    console.log(order);
+    
     return {
         addItem,
     }
