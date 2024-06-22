6aca678df6642b8707c9f0b19ebd091462f17498
Alex <alemumu207@gmail.com>
2024-06-22 13:39:21 +0200

    Cambiando useMemo por useCallback

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useMemo, useCallback } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
@@ -10,8 +10,9 @@ type OrderTotalsProps = {
 export default function OrderTotals({order, tip} : OrderTotalsProps) {
 
   // Solo se ejecuta useMemo cuando la dependencia cambie
-  const subtotalAmount = useMemo(() => order.reduce( (total, item) => total + (item.quantity * item.price), 0 ), [order] )
-  const tipAmount = useMemo( () => subtotalAmount * tip, [tip, order])
+  const subtotalAmount = useCallback(() => order.reduce( (total, item) => total + (item.quantity * item.price), 0 ), [order] )
+  const tipAmount = useCallback( () => subtotalAmount() * tip, [tip, order])
+  const totalAmount = useCallback( () => subtotalAmount() + tipAmount(), [tip, order])
 
   return (
     <>
@@ -19,15 +20,15 @@ export default function OrderTotals({order, tip} : OrderTotalsProps) {
         <h2 className="font-black text-2xl">Totales y propina:</h2>
         <p>
           Subtotal a pagar: {''}
-          <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+          <span className="font-bold">{formatCurrency(subtotalAmount())}</span>
         </p>
         <p>
           Propina a pagar: {''}
-          <span className="font-bold">{formatCurrency(tipAmount)}</span>
+          <span className="font-bold">{formatCurrency(tipAmount())}</span>
         </p>
         <p>
           Total a pagar: {''}
-          <span className="font-bold">$0</span>
+          <span className="font-bold">{formatCurrency(totalAmount())}</span>
         </p>
       </div>
 
